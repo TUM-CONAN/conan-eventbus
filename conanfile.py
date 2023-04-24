bc52c87f5ed6e651e8b57fb20b8f6357e2f332de
Ulrich Eck <ulrich.eck@tum.de>
2023-04-24 17:35:47 +0200

    new package for conan2: eventbus

--- conanfile.py
+++ conanfile.py
@@ -1,46 +1,71 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, replace_in_file, collect_libs
 import os
 
 
 class EventbusConan(ConanFile):
     name = "eventbus"
     version = "3.0.0-r2"
-    generators = "cmake"
-    settings = {"os": None, "arch": ["x86_64", "x86"], "compiler": None, "build_type": None}
-
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    exports_sources = "include*", "src*", "cmake*", "CMakeLists.txt"
-
-    exports = ["CMakeLists.txt"]
-
     license = "Licensed under the Apache License, Version 2.0"
     description = "Simple and very fast event bus."
 
-    scm = {"revision": "v3.0.0-r2",
-           "subfolder": "sources",
-           "type": "git",
-           "url": "https://github.com/TUM-CAMP-NARVIS/EventBus"}
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+    }
+    default_options = {
+        "shared": True,
+    }
 
-    def _cmake_configure(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.definitions["ENABLE_TEST"] = "OFF"
-        cmake.configure()
-        return cmake
-    
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "v{}".format(self.version),
+            "url": "https://github.com/TUM-CAMP-NARVIS/EventBus"
+        }})
+
+    def source(self):
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder)
+        git.checkout(commit=sources["commit"])
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
+
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
+
+        tc.cache_variables["ENABLE_TEST"] = False
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_folder")
+
     def build(self):
-        #disable building usecases
-        tools.replace_in_file(os.path.join("sources", "CMakeLists.txt"),
-            """add_subdirectory(use_case)""",
-            """#add_subdirectory(use_case)""")
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        """add_subdirectory(use_case)""",
+                        """#add_subdirectory(use_case)""")
 
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._cmake_configure()
+        copy(self, pattern="BOOST_1_0", dst="licenses", src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-        
+
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
